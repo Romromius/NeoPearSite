6aca202a422cd766e625805a66fda5e826e26fae
Romromius <navrosroman@gmail.com>
2025-07-18 23:21:57 +1000

    host fix

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def main_page():
     return '<div style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%)">NeoPear</div>'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
