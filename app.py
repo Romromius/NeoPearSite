cb2d3ff8f0db3e2745c7d1c759179ee2397c3903
Romromius <navrosroman@gmail.com>
2025-07-18 23:10:04 +1000

    neopear

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 import flask
+
+app = flask.Flask("NeoPear")
+
+@app.route('/')
+def main_page():
+    return '<div style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%)">NeoPear</div>'
+
+if __name__ == '__main__':
+    app.run()
